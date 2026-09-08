5a8f10fc21171505d39f66d67c389a01c5f11f38
Kimeunho <kimeunho@Kimeunhoui-MacBookAir.local>
2026-09-08 14:03:26 +0900

    feat: Terraform 네트워크 구성 추가 (서브넷 4개, IGW, 라우팅 테이블)

--- infra/main.tf
+++ infra/main.tf
@@ -26,3 +26,75 @@ resource "aws_vpc" "main" {
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "baseball-igw"
+  }
+}
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.0.0/24"
+  availability_zone       = "ap-northeast-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "baseball-public-a"
+  }
+}
+
+resource "aws_subnet" "public_c" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-northeast-2c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "baseball-public-c"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.10.0/24"
+  availability_zone = "ap-northeast-2a"
+
+  tags = {
+    Name = "baseball-private-a"
+  }
+}
+
+resource "aws_subnet" "pribate_c" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "ap-northeast-2c"
+
+  tags = {
+    Name = "baseball-private-c"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name = "baseball-public-rt"
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_c" {
+  subnet_id      = aws_subnet.public_c.id
+  route_table_id = aws_route_table.public.id
+}
